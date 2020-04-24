9ab642c408a354f2d7a123ceaf3e5bbf55042540
William Nguyen(Khanh Trinh) <49286034+LiteWilliamNguyen@users.noreply.github.com>
2020-04-24 14:27:29 -0700

    Add files via upload

--- Assignment0Octagon.java
+++ Assignment0Octagon.java
@@ -1,22 +1,42 @@
 package ITC155;
-import java.awt.*;
-
-
-/*
- *  Write a class named Octagon whose objects represent regular octagons (eight-sided
- *  polygons). Your class should implement the shape interface defined in this chapter,
- *  including methods for its area and perimeter. An octagon object is defined by its
- *  side length (you may need to search online to find formulas for the area and
- *  perimeter of a regular octagon.)
- */
 
 public class Assignment0Octagon {
+
 	public static void main(String[] args) {
-		DrawingPanel panel = new DrawingPanel(200,200);
-		Graphics g = panel.getGraphics();
-		int [] x = {69,131,175,175,131,69,25,25};
-		int [] y = {25,25,69,131,175,175,131,69};
-		g.drawPolygon(x,y,8);
-		
+
+		/*
+		 *  Write a class named Octagon whose objects represent regular octagons (eight-sided
+		 *  polygons). Your class should implement the shape interface defined in this chapter,
+		 *  including methods for its area and perimeter. An octagon object is defined by its
+		 *  side length (you may need to search online to find formulas for the area and
+		 *  perimeter of a regular octagon.)
+		 */
 	}
-}
+			public interface Shape{
+				public double getArea();
+				public double getPerimeter();
+			}
+			// Represent octagon shapes
+			public class Octagon implements Shape {
+			private double base;
+			
+			 // Constructs a new octagon with given dimension
+			public Octagon(double base) {
+				this.base = base;
+			}
+			
+			// Returns the area of this octagon
+			public double getArea() {
+				return base*base*2*(1+ Math.sqrt(2));
+			}
+			
+			//return the perimeter of this octagon
+			public double getPerimeter() {
+				return 8*base;
+			}
+			
+			
+			}
+			}
+
+
